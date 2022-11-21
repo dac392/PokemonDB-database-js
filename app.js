27959e82878b639f4d33ae91f64e55429f3db0b9
dac392 <dac392@scarletmail.rutgers.edu>
2022-11-21 18:29:58 -0500

    functional db

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import inquirer from 'inquirer';
 
 let pokemon_name;
 const stats = {};
+const stat_tracker = [];
 
 async function ask_pokemon() {
     const answers = await inquirer.prompt({
@@ -36,6 +37,89 @@ async function get_pokemon(){
     stats["special-attack"] = pokemon.stats[3].base_stat; 
     stats["special-defense"] = pokemon.stats[4].base_stat; 
     stats["speed"] = pokemon.stats[5].base_stat; 
+
+}
+
+function color_wraper(stats_key){
+    const value = stats[stats_key];
+    if(value >= 90){
+        return chalk.green(`${value} (good)`);
+    }else if( value >= 70){
+        return chalk.yellow(`${value} (alright)`);
+    }else{
+        return chalk.red(`${value} (bad)`);
+    }
+}
+
+function total_base_stats(){
+    const total = stats["hp"]+stats["attack"]+stats["defense"]+stats["special-attack"]+stats["special-defense"]+stats["speed"];
+    if(total >= 500){
+        return chalk.green(`${total} (good)`);
+    }else if( total >= 400){
+        return chalk.yellow(`${total} (alright)`);
+    }else{
+        return chalk.red(`${total} (meh)`);
+    }
+}
+function nextBiggest(arr) {
+    arr.sort((a,b)=>b-a);
+    return arr[1];
+}
+
+function stat_focus(){
+    const hp = stats["hp"];
+    const attack = stats["attack"];
+    const defense = stats["defense"];
+    const sp_attack = stats["special-attack"];
+    const sp_defense = stats["special-defense"];
+    const speed = stats["speed"];
+
+    const focus_group = [hp, attack, defense, sp_attack, sp_defense, speed].sort((a,b)=>b-a);
+    focus_group.splice(2);
+
+    const result = [];
+    for(let val of focus_group){
+        switch(val){
+            case hp:
+                result.push("hp");
+                break;
+            case attack:
+                result.push("attack");
+                break;
+            case defense:
+                result.push("defense");
+                break;
+            case sp_attack:
+                result.push("special-attack");
+                break;
+            case sp_defense:
+                result.push("special-defense");
+                break;
+            case speed:
+                result.push("speed");
+                break;
+        }
+    }
+    return result;
+}
+
+async function form_response(){
+    console.log(`
+name:               ${stats["name"]}
+id:                 ${stats["id"]}
+types:              ${stats["types"]}
+
+hp:                 ${color_wraper("hp")}
+attack:             ${color_wraper("attack")}
+defense:            ${color_wraper("defense")}
+spedial attack:     ${color_wraper("special-attack")}
+special defense:    ${color_wraper("special-defense")}
+speed:              ${color_wraper("speed")}
+
+total:              ${total_base_stats()}
+
+focus:              ${stat_focus()}
+    `);
 }
 
 
@@ -45,4 +129,5 @@ async function get_pokemon(){
 console.clear();
 await ask_pokemon();
 await get_pokemon();
+console.clear();
 await form_response();
