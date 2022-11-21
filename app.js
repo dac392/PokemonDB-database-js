f8b6ccef5e471fa5be6b836e9ccd405873d81636
dac392 <dac392@scarletmail.rutgers.edu>
2022-11-21 17:08:01 -0500

    able to fetch information

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import chalk from 'chalk';
 import inquirer from 'inquirer';
 
 let pokemon_name;
+const stats = {};
+
 async function ask_pokemon() {
     const answers = await inquirer.prompt({
       name: 'pokemon_name',
@@ -12,8 +14,28 @@ async function ask_pokemon() {
       },
     });
   
-    pokemon_name = answers.pokemon_name;
+    pokemon_name = answers.pokemon_name.toLowerCase();
     console.log(pokemon_name);
+}
+
+async function get_pokemon(){
+    const res= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_name}`);
+    const pokemon = await res.json();
+
+    const types = []
+    for(let obj of pokemon.types){
+        types.push(obj.type.name);
+    }
+    
+    stats["name"] = pokemon_name;
+    stats["id"] = pokemon.id;
+    stats["types"] = types;
+    stats["hp"] = pokemon.stats[0].base_stat; 
+    stats["attack"] = pokemon.stats[1].base_stat; 
+    stats["defense"] = pokemon.stats[2].base_stat; 
+    stats["special-attack"] = pokemon.stats[3].base_stat; 
+    stats["special-defense"] = pokemon.stats[4].base_stat; 
+    stats["speed"] = pokemon.stats[5].base_stat; 
 }
 
 
@@ -22,3 +44,5 @@ async function ask_pokemon() {
  */
 console.clear();
 await ask_pokemon();
+await get_pokemon();
+await form_response();
